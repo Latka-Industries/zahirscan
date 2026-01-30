cd8e0ac842fee051badaba3dc12475c19942ea17
AHurowitz <thicclatka@users.noreply.github.com>
2026-01-30 16:30:32 -0500

    refactor(tests): simplify test module structure

---
+++ tests/unit_tests/mod.rs
@@ -0,0 +1,13 @@
+//! Unit tests for ZahirScan core functionality
+
+mod jpeg_subsampling;
+mod filename_sanitization;
+mod adaptive_chunking;
+mod minimal_fallback;
+mod serialization;
+mod placeholder_formatting;
+mod csv_metadata;
+mod image_metadata;
+mod sqlite_metadata;
+mod code_metadata;
+mod should_ignore_path;
